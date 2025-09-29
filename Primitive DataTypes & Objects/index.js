ccb45eef810426a8ad60456eafb5ed269f4346f4
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-09-29 19:22:23 +0530

    Add files via upload

---
+++ Primitive DataTypes & Objects/index.js
@@ -0,0 +1,72 @@
+/* 
+
+DATA-TYPES IN JAVASCRIPT
+
+    N - Null
+    N - Number
+    S - Symbol
+    S - String
+    B - Boolean
+    B - Bigint
+    U - Undefined
+
+*/
+
+// PRIMITIVE 
+
+/*
+let a = null;
+let b = 333;
+let c = true;
+let d = BigInt("696969");
+let e = "SAM";
+let f = Symbol("I'm a nice symbol");
+let g = undefined;
+
+let h = BigInt("352") + BigInt(898);
+
+console.log(a);
+console.log("Type of this variable is => ",typeof a);
+console.log(); //For a Line space
+
+console.log(b);
+console.log("Type of this variable is => ",typeof b);
+console.log(); //For a Line space
+
+console.log(c);
+console.log("Type of this variable is => ",typeof c);
+console.log(); //For a Line space
+
+console.log(d);
+console.log("Type of this variable is => ",typeof d);
+console.log(); //For a Line space
+
+console.log(e);
+console.log("Type of this variable is => ",typeof e);
+console.log(); //For a Line space
+
+console.log(f);
+console.log("Type of this variable is => ",typeof f);
+
+console.log(); //For a Line space
+
+console.log(g);
+
+console.log(); //For a Line space
+
+console.log(h);
+console.log("Type of this variable is => ",typeof h);
+*/
+
+// OBJECTS
+
+const item = {
+    "A": true,
+    "B": false,
+    "C": 69,
+    "D": undefined
+}
+
+console.log(item["A"])
+
+console.log(item["sam"])
