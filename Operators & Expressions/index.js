33f48eb40e36459e6ad0f08230d56046a21a8427
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-10-02 23:03:03 +0530

    Add files via upload

---
+++ Operators & Expressions/index.js
@@ -0,0 +1,76 @@
+console.log("Operators in JS")
+
+console.log();
+
+// Arithemetic Operators
+
+    // let a = 45, b = 5;
+
+    // console.log("a + b = ", a+b);
+    // console.log("a - b = ", a-b);
+    // console.log("a * b = ", a*b);
+    // console.log("a / b = ", a/b);
+    // console.log("a ** b = ", a**b);
+    // console.log("a % b = ", a%b);
+    // console.log("Post Increment on a = ", a++);
+    // console.log("Pre Increment on a = ", ++a);
+    // console.log("Post Decrement on a = ", a--);
+    // console.log("Pre Decrement on a = ", --a);
+
+//Assignment Operators
+
+    // let x = 1;
+
+    // x += 5
+    // x-=5
+    // x*=5
+    // x/=5
+    // x%=5
+    // x**=5
+    // console.log(x);
+
+//Comparison Operator
+
+    // let comp1 = 6;
+    // let comp2 = 5;
+
+    // let comp3 = 8;
+    // let comp4 = 8;
+
+    // let comp5 = 10;
+    // let comp6 = 2;
+
+    // let comp7 = 20;
+    // let comp8 = 91;
+
+    // console.log("comp1 = comp2 is ",comp1 == comp2);
+    // console.log("comp3 = comp4 is ",comp3 == comp4);
+    // console.log();
+    // console.log("comp1 != comp2 is ",comp1 != comp2);
+    // console.log("comp3 != comp4 is ",comp3 != comp4);
+    // console.log();
+    // console.log("comp1 === comp2 is ",comp1 === comp2);
+    // console.log("comp3 !== comp4 is ",comp3 !== comp4);
+    // console.log();
+    // console.log("comp1 !== comp2 is ",comp1 !== comp2);
+    // console.log("comp3 === comp4 is ",comp3 === comp4);
+    // console.log();
+    // console.log("comp5 > comp6 is ",comp5 > comp6);
+    // console.log("comp7 < comp8 is ",comp7 < comp8);
+    // console.log();
+    // console.log("comp3 >= comp4 is ",comp5 >= comp6);
+    // console.log("comp7 <= comp2 is ",comp7 <= comp8);
+
+//Logical Operators
+
+    let x = 5;
+    let y = 6;
+
+    console.log(x<y && x==5);
+    console.log(x<y || x==5);
+
+    console.log();
+
+    console.log(!false);
+    console.log(!true);
+    
