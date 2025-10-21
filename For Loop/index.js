76de0c4baff1719478a6423fff0361e1779da091
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-10-21 13:52:02 +0530

    For Loop , For-in Loop & For of-Loop in JS

---
+++ For Loop/index.js
@@ -0,0 +1,43 @@
+// for(let i=0; i<=34; i++){
+//     console.log(i);
+// }
+
+const prompt = require("prompt-sync")();
+
+/*
+let sum = 0;
+let n = prompt("Enter the value of n = ")
+n = Number.parseInt(n)
+
+// for(let i=0; i<50; i++){
+//     console.log(i+1);
+// }
+
+for(let i=0; i<n; i++){
+    sum += (i+1);
+}
+console.log("Sum of first "+ n + " natural numbers is "+ sum);
+*/
+
+
+/*
+//for-in loop & for-of loop
+
+let marks = {
+    Samarth: 100,
+    Sam: 95,
+    Srk: 90,
+    Dev: 85,
+    Sammy: 80
+}
+
+//for-in loop
+for(let a in marks){
+    console.log("Marks of " + a + " are " + marks[a]);
+}
+
+//for-of loop
+for(let b of "Samarth"){
+    console.log(b)
+}
+*/
